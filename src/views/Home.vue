b4f1f6b70e6fa3e40ca0f6da258934d6dc1177ed
choigawoon <choigawoon@nexon.co.kr>
2023-07-25 19:42:10 +0900

    배포 테스트할 페이지 간단 작성 gitignore에 typescript 생성정보는 제외처리

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,27 @@
-<template>
-  <HelloWorld />
-</template>
 
 <script lang="ts" setup>
   import HelloWorld from '@/components/HelloWorld.vue'
+
+  const clicked = async() => {
+    window.alert('test')
+
+    const temp = localStorage.getItem('test')
+    console.error(`temp : ${temp}`)
+    localStorage.setItem('test', 'test')
+  }
 </script>
+
+
+<template>
+  <v-card hover>
+    <v-card-title>Test</v-card-title>
+    <v-card-text>Contents #1
+      Contents #2
+      Contents #3
+    </v-card-text>
+    <v-card-actions>
+      <v-btn @click="clicked">Click</v-btn>
+    </v-card-actions>
+  </v-card>
+  <HelloWorld />
+</template>
